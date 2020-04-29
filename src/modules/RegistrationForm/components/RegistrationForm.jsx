c2e6aa82da6691008c82bb543b83968d30c8a5c0
Dafaq <a.kuchuk@uptrader.io>
2020-04-29 22:24:38 +0300

    added success block

--- src/modules/RegistrationForm/components/RegistrationForm.jsx
+++ src/modules/RegistrationForm/components/RegistrationForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Form } from 'antd'
-import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons'
+import { UserOutlined, LockOutlined, MailOutlined, ExclamationCircleTwoTone } from '@ant-design/icons'
 import { Block, Button, Input } from 'components'
 
 const RegistrationForm = () => {
@@ -9,72 +9,84 @@ const RegistrationForm = () => {
     console.log(values)
   }
 
+  const success = false
+
   return (
-    <div>
+    <div className="RegistrationForm">
       <div className="Auth__header">
         <h2>Регистрация</h2>
         <p>Для входа в чат, вам необходимо зарегистрироваться</p>
       </div>
       <Block>
-        <Form name="normal_login" className="login-form" onFinish={onFinish}>
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: 'Введите почту',
-              },
-            ]}
-            type="email">
-            <Input prefix={<MailOutlined className="site-form-item-icon" />} placeholder="E-mail" size="large" />
-          </Form.Item>
-          <Form.Item
-            name="name"
-            rules={[
-              {
-                required: true,
-                message: 'Введите ваше имя',
-              },
-            ]}>
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Ваше имя" size="large" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Введите пароль',
-              },
-            ]}
-            type="password">
-            <Input prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Пароль" size="large" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Введите пароль',
-              },
-            ]}
-            type="password">
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              placeholder="Повторить пароль"
-              size="large"
-            />
-          </Form.Item>
+        {!success ? (
+          <Form name="normal_login" className="login-form" onFinish={onFinish}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Введите почту',
+                },
+              ]}
+              type="email">
+              <Input prefix={<MailOutlined className="site-form-item-icon" />} placeholder="E-mail" size="large" />
+            </Form.Item>
+            <Form.Item
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Введите ваше имя',
+                },
+              ]}>
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Ваше имя" size="large" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Введите пароль',
+                },
+              ]}
+              type="password">
+              <Input prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Пароль" size="large" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Введите пароль',
+                },
+              ]}
+              type="password">
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Повторить пароль"
+                size="large"
+              />
+            </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" size="large">
-              Зарегистрироваться
-            </Button>
-          </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" size="large">
+                Зарегистрироваться
+              </Button>
+            </Form.Item>
 
-          <Link className="Auth__registration-link" to="/login">
-            Войти в аккаунт
-          </Link>
-        </Form>
+            <Link className="Auth__registration-link" to="/login">
+              Войти в аккаунт
+            </Link>
+          </Form>
+        ) : (
+          <div className="Auth__success">
+            <div>
+              <ExclamationCircleTwoTone />
+            </div>
+            <h2>Подтвердите свой аккаунт</h2>
+            <p>На Вашу почту было отправлено письмо со ссылкой для подтверждения аккаунта.</p>
+          </div>
+        )}
       </Block>
     </div>
   )
