28de2fe9f30ae94d27146c9642e2741bd4da1bd1
Dafaq <a.kuchuk@uptrader.io>
2020-04-30 14:50:57 +0300

    added readed icons

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -4,10 +4,13 @@ import classNames from 'classnames'
 import formatDistance from 'date-fns/formatDistance'
 import ruLocale from 'date-fns/locale/ru'
 
+import readedIcon from 'assets/img/readedIcon.svg'
+import checkIcon from 'assets/img/checkIcon.svg'
+
 import './Message.scss'
 
-const Message = ({ avatar, user, text, date, isMine }) => {
-  const beatyDate = formatDistance(date, new Date(), {
+const Message = ({ avatar, user, text, date, isMine, isReaded }) => {
+  const beautyDate = formatDistance(date, new Date(), {
     addSuffix: true,
     locale: ruLocale,
     includeSeconds: true,
@@ -15,14 +18,21 @@ const Message = ({ avatar, user, text, date, isMine }) => {
 
   return (
     <div className={classNames('Message', { 'Message--my': isMine })}>
-      <div className="Message__avatar">
-        <img src={avatar} alt={`Avatar ${user.fullName}`} />
-      </div>
       <div className="Message__content">
-        <div className="Message__bubble">
-          <p className="Message__text">{text}</p>
+        <div className="Message__avatar">
+          <img src={avatar} alt={`Avatar ${user.fullName}`} />
         </div>
-        <span className="Message__date">{beatyDate}</span>
+        <div className="Message__info">
+          <div className="Message__bubble">
+            <p className="Message__text">{text}</p>
+          </div>
+          <span className="Message__date">{beautyDate}</span>
+        </div>
+        {isMine && (
+          <div className="Message__checked">
+            <img src={isReaded ? readedIcon : checkIcon} alt="" />
+          </div>
+        )}
       </div>
     </div>
   )
@@ -34,11 +44,13 @@ Message.propTypes = {
   text: PropTypes.string,
   date: PropTypes.number,
   isMine: PropTypes.bool,
+  isReaded: PropTypes.bool,
 }
 
 Message.defaultProps = {
   user: {},
   isMine: false,
+  isReaded: false,
 }
 
 export default Message
