7746d042816e8bc3727ce9a25e576ba1f73d4907
Dafaq <a.kuchuk@uptrader.io>
2020-05-04 19:31:36 +0300

    added validation

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,34 +1,50 @@
-const { Router } = require('express')
-const router = Router()
-const bycrypt = require('bcrypt')
+const { Router } = require('express');
+const router = Router();
+const bycrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
 
-const User = require('../models/User')
-const logger = require('../helpers/logger')
+const User = require('../models/User');
+const logger = require('../helpers/logger');
 
-router.post('/registration', async (req, res) => {
-  try {
-    const { email, fullName, password } = req.body.fullName
+router.post(
+  '/registration',
+  [
+    check('email', 'Email is invalid').isEmail(),
+    check('password', 'Min length is 6 chars').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const candidate = await User.findOne({ email })
+      if (!errors.isEmpty()) {
+        return res
+          .status(400)
+          .json({ errors: errors.array(), message: 'Incorrect data' });
+      }
 
-    if (candidate) {
-      logger.warn('user exist', candidate)
-      return res.status(400).json({ message: 'User already exists' })
+      const { email, fullName, password } = req.body.fullName;
+
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        logger.warn('user exist', candidate);
+        return res.status(400).json({ message: 'User already exists' });
+      }
+
+      const hashedPass = await bycrypt.hash(password, 12);
+      const user = new User({ email, fullName, password: hashedPass });
+
+      await user.save();
+
+      logger.info('user created', candidate);
+      return res.status(201).json({ message: 'User succefull created!' });
+    } catch (error) {
+      logger.error('reg err', error);
+      res.status(500).json({ message: 'Error on registration' });
     }
-
-    const hashedPass = await bycrypt.hash(password, 12)
-    const user = new User({ email, fullName, password: hashedPass })
-
-    await user.save()
-
-    logger.info('user created', candidate)
-    return res.status(201).json({ message: 'User succefull created!' })
-  } catch (error) {
-    logger.error('reg err', error)
-    res.status(500).json({ message: 'Error on registration' })
   }
-})
+);
 
-router.post('/login', async (req, res) => {})
+router.post('/login', async (req, res) => {});
 
-module.exports = router
+module.exports = router;
